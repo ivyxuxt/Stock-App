f1da2a130bf446426709937e2894fc4b6c2206ca
Ivy Xu <ivyxu@Ivys-MacBook-Air.local>
2024-12-16 10:44:23 +0800

    first commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
 import yfinance as yf
 from keras.models import load_model # type: ignore
 import streamlit as st
